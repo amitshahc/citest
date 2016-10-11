5019fc73b9805c33ede5755b75e46c1a8966dd6a
amitshahc <amitshahc@hotmail.com>
2016-10-12 00:20:09 +0800

    Initial Commit

---
+++ application/views/common/Header.php
@@ -0,0 +1,21 @@
+<?php echo doctype('html5'); ?>
+<html>
+<head>
+	<title></title>
+	
+	<?php
+
+	$meta = array(
+        array('name' => 'robots', 'content' => 'no-cache'),
+        array('name' => 'description', 'content' => 'My Great Site'),
+        array('name' => 'keywords', 'content' => 'love, passion, intrigue, deception'),
+        array('name' => 'robots', 'content' => 'no-cache'),
+        array('name' => 'Content-type', 'content' => 'text/html; charset=utf-8', 'type' => 'equiv')
+    );
+
+	echo meta($meta); 
+	?>
+	
+</head>
+<body>
+
